e3199c794271f5c6b27eded62265cf9513011c51
davewinnyk <70180828+davewinnyk@users.noreply.github.com>
2021-02-19 23:23:18 -0600

    Update quickstart.js

--- quickstart.js
+++ quickstart.js
@@ -8,7 +8,7 @@
   var device;
 
   log('Requesting Capability Token...');
-  $.getJSON('/token')
+  $.getJSON('https://rosewood-camel-7181.twil.io/capability-token')
     .then(function (data) {
       log('Got a token.');
       console.log('Token: ' + data.token);
